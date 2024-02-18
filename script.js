3acad7233ddf97b175ddf5967a5fe12f8e331b50
Lavinia200 <160424375+Lavinia200@users.noreply.github.com>
2024-02-18 22:21:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function openAcasa(){
+
+    window.location.href=" index.html";
+
+}
+
+function openContact(){
+    window.location.href=" contact.html";
+    
+}
+
+function openTehnologie(){
+
+    window.location.href=" tehnologie.html";
+    
+}
+
+function openSanatate(){
+
+    window.location.href=" sanatate.html";
+    
+}
+
+function openClima(){
+
+    window.location.href=" clima.html";
+    
+}
+function openTechReviews(){
+
+    window.location.href=" TechReviews.html";
+    
+}
+function openStire1_clima(){
+
+    window.location.href=" Stire1_clima.html";
+    
+}
+function openStire1_tehnologie(){
+
+    window.location.href=" Stire1_tehnologie.html";
+    
+}
+function openStire2_tehnologie(){
+
+    window.location.href=" Stire2_tehnologie.html";
+    
+}
+function openStire1_sanatate(){
+
+    window.location.href=" Stire1_sanatate.html";
+    
+}
